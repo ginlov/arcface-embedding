e2b0dbf5de1abefa2c5edb6ba2cb467a28b744d9
Long Giang Vu <giangvl.cs@gmail.com>
2025-10-27 00:52:54 -0700

    feat: update

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #/bin/bash
+mkdir -p /root/.insightface/models
+export INSIGHTFACE_ROOT=/root/.insightface/models
+wget https://github.com/deepinsight/insightface/releases/download/v0.7/antelopev2.zip -0 /root/.insightface/models/antelopev2.zip
+
+unzip /root/.insightface/models/antelopev2.zip -d /root/.insightface/models/
+
+rm /root/.insightface/models/antelopev2.zip
 
 python embedding-arcface.py
